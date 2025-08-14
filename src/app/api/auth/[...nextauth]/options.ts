9cfd9cc778ab02ec86da3fba9f874b8128f4c39c
Vinodbiradar09 <vinodjb07@gmail.com>
2025-08-15 01:22:39 +0530

    accept-msg

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -59,15 +59,15 @@ export const authOptions : NextAuthOptions= {
         return token;
        },
 
-       async session({session , token}){
-        if(token){
-            session.user._id = token._id?.toString();
-            session.user.isVerified = token.isVerified;
-            session.user.isAcceptingMessages = token.isAcceptingMessages;
-            session.user.username = token.username;
-        }
-        return session
-       }
+       async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
     },
     pages : {
         signIn : '/sign-in',
