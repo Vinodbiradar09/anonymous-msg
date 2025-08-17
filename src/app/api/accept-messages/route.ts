2ee6e270ba868497b12f8b90f3f121bc72203d37
Vinodbiradar09 <vinodjb07@gmail.com>
2025-08-17 15:57:44 +0530

    dashboard

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -5,6 +5,7 @@ import dbConnect from "@/lib/dbConnect";
 import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
 import { User } from "next-auth";
 
+
 export async function POST(request: Request) {
     await dbConnect();
 
@@ -76,7 +77,7 @@ export async function POST(request: Request) {
     }
 }
 
-export async function GET(request: Request) {
+export async function GET(request: Request){
     await dbConnect();
 
     const session = await getServerSession(authOptions);
